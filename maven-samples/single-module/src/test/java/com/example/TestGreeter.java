baf9c3048a0d9f1c5cc0ae735ca6be9d6c25a0ef
TonkawZaa <tonkaw.zaa@gmail.com>
2020-01-26 17:35:56 +0700

    [TEST] Add TEST Me

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -32,6 +32,13 @@ public class TestGreeter {
   }
 
     @Test
+  public void greetShouldIncludeGreetingPhrase2() {
+    String someone = "Hello World";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+
+    @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
 
